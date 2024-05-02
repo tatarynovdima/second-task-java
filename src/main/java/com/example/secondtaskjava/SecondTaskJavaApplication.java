b443850d6455e026f78000a0c80122e947bf9584
tatarynovdima <dmytro.course@gmail.com>
2024-05-02 21:00:25 +0200

    Initial commit

---
+++ src/main/java/com/example/secondtaskjava/SecondTaskJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondtaskjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondTaskJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondTaskJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/secondtaskjava/SecondTaskJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondtaskjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondTaskJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
